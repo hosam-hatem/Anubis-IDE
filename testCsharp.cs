bef9d3624ff12104b2fccbde725cf8d944c52e4d
hosam-hatem <30728813+hosam-hatem@users.noreply.github.com>
2022-06-25 22:15:02 +0200

    Add files via upload

---
+++ testCsharp.cs
@@ -0,0 +1,17 @@
+/* try
+ * multiline
+ * comment */
+// try single comment
+
+namespace CSharpTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World!!";
+
+            Console.WriteLine(message);
+        }
+    }
+}
